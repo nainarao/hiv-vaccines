b1ea0eb05d2cada53651ed3d49fcc2b12105e83d
Rao <nrao6@bwh.harvard.edu>
2018-09-06 15:01:39 -0400

    can access  rave sandbox and list subjects. medidata not authenticating

--- 127diary.py
+++ 127diary.py
@@ -1,16 +1,20 @@
 from rwslib import RWSConnection
-from rwslib.rws_requests import MetadataStudiesRequest, VersionRequest, DiagnosticsRequest, ClinicalStudiesRequest, StudySubjectsRequest
+from rwslib.rws_requests import MetadataStudiesRequest
+from rwslib.rws_requests import VersionRequest, DiagnosticsRequest,  ClinicalStudiesRequest
 
 
 app_id = '635r8aib-21e9-6b5f-867e-bk2358ub2784'
 key = open('keypair_dir/vaccines_mauth.priv.key','r').read()
 
-rws = RWSConnection('https://innovate.mdsol.com', "nrao1","H4Vaccine!")
+#rws = RWSConnection('innovate', "nrao1","H4Vaccine!")
+rws = RWSConnection('scharp', "nrao1","H4Vaccine!")
 
 
 #rws.send_request(MetadataStudiesRequest())
 
+
 print rws.send_request(VersionRequest())
+print "url: ", rws.last_result.url
 print "Status code: ", rws.last_result.status_code
 print "diagnostics: ", rws.send_request(DiagnosticsRequest())
 
@@ -24,7 +28,3 @@ for study in studies:
     print "Name",study.studyname
     print "protocolname",study.protocolname
     print "IsProd?",study.isProd()
-
-subject_list = rws.send_request(StudySubjectsRequest("Mediflex", "Dev"))
-for subject in subject_list:
-    print "Name: %s" % subject.subjectkey
