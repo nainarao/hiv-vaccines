b47df159ac09c461c180a585f52f45a76ee747e4
Rao <nrao6@bwh.harvard.edu>
2018-09-11 13:42:26 -0400

    adding subject works

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+import sys
 from rwslib import RWSConnection
 from rwslib.builders import *
 from rwslib.rws_requests import MetadataStudiesRequest, VersionRequest, DiagnosticsRequest, ClinicalStudiesRequest, StudySubjectsRequest, StudyDatasetRequest, SubjectDatasetRequest, PostDataRequest
+from rwslib.rwsobjects import *
 import xml.dom.minidom
 import os
 import re
@@ -59,18 +61,20 @@ def printAllSubjects():
     subject_list = rws.send_request(StudySubjectsRequest(project_name, enviro_name))
     print "Subjects in Study: "
     for subject in subject_list:
-        print "Name: %s (%s)" % (subject.subject_name, subject.subjectkey)
+        #print "Name: %s (%s)" % (subject.subjectname, subject.subjectkey)
+        print "Name: %s" % (subject.subjectkey)
 
-def addNewSubject(sub_initals, sub_id):
+def addNewSubject(sub_key, sub_id, sub_init):
     print "add new subject original"
     odm = ODM("test system")(
         ClinicalData(project_name, enviro_name)(
-            SubjectData(location_oid,"New Subject", "Insert")(
-                StudyEventData("Subject")(
+            SubjectData(site_location_oid=location_oid, subject_key=sub_key, transaction_type="Insert")(
+                StudyEventData("SUBJECT")(
                     FormData("EN", transaction_type="Update")(
                         ItemGroupData()(
-                            ItemData("SUBJID",sub_id),
-                            ItemData("SUBJINIT",sub_initals)
+                            ItemData("SUBJID",sub_key),
+                            ItemData("SUBJINIT",sub_init),
+                            ItemData("USUBJID", sub_id)
                         )
                     )
                 )
@@ -82,14 +86,42 @@ def addNewSubject(sub_initals, sub_id):
     print "Addition Successful: ", resp.istransactionsuccessful
     print "Fields Changed: \n", str(resp)
 
+def removeSubject(subjectID):
+    data = """<ODM xmlns="http://www.cdisc.org/ns/odm/v1.3" ODMVersion="1.3" FileType="Transactional" FileOID="Example-1" CreationDateTime="2008-01-01T00:00:00"> <ClinicalData StudyOID="Mediflex (Dev)" MetaDataVersionOID="1"><SubjectData SubjectKey=\""""+subjectID +"""" TransactionType="Remove"><SiteRef LocationOID="BWH0001"/></SubjectData></ClinicalData></ODM>"""
+    print data
+    resp = rws.send_request(PostDataRequest(data))
+    print "Addition Successful: ", resp.istransactionsuccessful
+
 def addNewSubject1():
     print "add new subject1"
-    data = """<ODM xmlns="http://www.cdisc.org/ns/odm/v1.3" ODMVersion="1.3" FileType="Transactional" FileOID="Example-1" CreationDateTime="2008-01-01T00:00:00"> <ClinicalData StudyOID="Mediflex (Dev)" MetaDataVersionOID="1"><SubjectData SubjectKey="123 ABC" TransactionType="Insert"><SiteRef LocationOID="BWH0001"/></SubjectData></ClinicalData></ODM>"""
-    # data = """<?xml version="1.0" encoding="utf-8" ?><ODM CreationDateTime="2013-06-17T17:03:29" FileOID="3b9fea8b-e825-4e5f-bdc8-1464bdd7a664" FileType="Transactional" ODMVersion="1.3" Originator="test system" xmlns="http://www.cdisc.org/ns/odm/v1.3" xmlns:mdsol="http://www.mdsol.com/ns/odm/metadata"><ClinicalData MetaDataVersionOID="1" StudyOID="Mediflex (Dev)"> <SubjectData SubjectKey="New Subject" TransactionType="Insert"> <SiteRef LocationOID="MDSOL" /> <StudyEventData StudyEventOID="SUBJECT"> <FormData FormOID="EN" FormRepeatKey="1" TransactionType="Update"> <ItemGroupData ItemGroupOID="EN" mdsol:Submission="SpecifiedItemsOnly"> <ItemData ItemOID="SUBJID" Value="1" /> <ItemData ItemOID="SUBJINIT" Value="AAA" /> </ItemGroupData> </FormData> </StudyEventData> </SubjectData> </ClinicalData> </ODM>"""
+
+    data = """<?xml version="1.0" encoding="utf-8" ?>
+    <ODM CreationDateTime="2013-06-17T17:03:29" FileOID="3b9fea8b-e825-4e5f-bdc8-1464bdd7a664"
+         FileType="Transactional" ODMVersion="1.3"
+         Originator="test system"
+         xmlns="http://www.cdisc.org/ns/odm/v1.3" xmlns:mdsol="http://www.mdsol.com/ns/odm/metadata">
+      <ClinicalData MetaDataVersionOID="1" StudyOID="Mediflex (DEV)">
+        <SubjectData SubjectKey="ABC" TransactionType="Update">
+          <SiteRef LocationOID="BWH0001" />
+          <StudyEventData StudyEventOID="SUBJECT">
+            <FormData FormOID="EN" FormRepeatKey="1" TransactionType="Update">
+              <ItemGroupData ItemGroupOID="EN" mdsol:Submission="SpecifiedItemsOnly">
+                <ItemData ItemOID="SUBJID" Value="ABC"/>
+                <ItemData ItemOID="SUBJINIT" Value="123ABC" />
+                <ItemData ItemOID="EN.USUBJID" Value="abc"/>
+              </ItemGroupData>
+            </FormData>
+          </StudyEventData>
+        </SubjectData>
+      </ClinicalData>
+    </ODM>"""
+
     print data
     resp = rws.send_request(PostDataRequest(data))
     print "Addition Successful: ", resp.istransactionsuccessful
     print "Fields Changed: \n", str(resp)
+
+#def updateSubjectDiary(sub_key):
 
 def makeODM():
     print "make ODM: "
@@ -100,7 +132,7 @@ def makeODM():
     clinical_data = ClinicalData("Mediflex", "DEV")
 
     # Subject Site, Subject Name and the transaction type
-    subject_data = SubjectData("MDSOL", "New Subject", "Insert")
+    subject_data = SubjectData(site_location_oid=location_oid, subject_key="xyz", transaction_type="Insert")
 
     # The special "SUBJECT" event represents subject-level forms
     event_data = StudyEventData("SUBJECT")
@@ -113,7 +145,8 @@ def makeODM():
 
     # Push itemdata elements into the itemgroup
     itemgroup << ItemData("SUBJINIT","AAA")
-    itemgroup << ItemData("SUBJID",001)
+    #itemgroup << ItemData("SUBJID",001)
+    itemgroup << ItemData("USUBJID", "xxx")
 
     # Now we put it all together
     odm << clinical_data << subject_data << event_data << form_data << itemgroup
@@ -123,6 +156,9 @@ def makeODM():
 
     # Print a string representation of the ODM document
     print(str(odm))
+    resp = rws.send_request(PostDataRequest(str(odm)))
+    print "Addition Successful: ", resp.istransactionsuccessful
+    print "Fields Changed: \n", str(resp)
 
 app_id = '635r8aib-21e9-6b5f-867e-bk2358ub2784'
 key = open('keypair_dir/vaccines_mauth.priv.key','r').read()
@@ -131,10 +167,15 @@ rws = RWSConnection('https://innovate.mdsol.com', "nrao1","H4Vaccine!")
 
 #testConnection()
 #printSubjectData("123")
-printAllSubjects()
-# addNewSubject("ABC", 003)
-addNewSubject1()
-printAllSubjects()
+
+
+if len(sys.argv) > 1:
+    printSubjectData(sys.argv[1])
+    #removeSubject(sys.argv[1])
+#printAllSubjects()
+#makeODM()
+#addNewSubject("z", 003, "abc")
+#addNewSubject1()
+#printAllSubjects()
 
 #printDataset()
-#printSubjectData('1')
